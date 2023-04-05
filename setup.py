e141073a7f167b84b03e718b892bdbac595f32cc
yevhenii.dehtiar <yevhenii.dehtiar@servicehouse.nl>
2023-04-05 16:00:36 +0200

    fix the name in setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="fastapi-cognito",
+    name="cbe-fastapi-cognito",
     version="2.3.0",
     author="Marko Mirosavljev",
     author_email="mirosavljevm023@gmail.com",
